856734fec65f640b71e55231afc1753361248633
Akbar <akbarsafarov.as@gmail.com>
2024-02-19 21:48:58 +0500

    map

--- js/main.js
+++ js/main.js
@@ -413,3 +413,18 @@ $(document).ready(function(){
     })
 
 })
+
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [55.755814, 37.617635], 
+        zoom: 10,
+        controls: ['zoomControl']
+    });
+
+    myMap.events.add('wheel', function(e) {
+        e.preventDefault();
+    });
+};
